4a9343102557f84028b94464410b7ba6662746e8
zheng.liang <zheng.liang@17daxue.com>
2019-06-14 16:52:00 +0800

    文章联动

--- src/views/web/MedicalNews/info.vue
+++ src/views/web/MedicalNews/info.vue
@@ -6,31 +6,31 @@
       <router-link :to="'/news'">医疗新闻</router-link>
       >
       <router-link :to="'/news'">肿瘤新闻</router-link>
-      > 阿法替尼与上代肺癌靶向药之间差异大吗?
+      > {{medicalArticle.title}}
     </div>
     <div class="content">
       <div class="main">
         <div class="title-info">
-          <div style="font-size: 2rem;">阿法替尼与上代肺癌靶向药之间差异大吗?</div>
+          <div style="font-size: 2rem;">{{medicalArticle.title}}</div>
           <div style="font-size: 1.2rem">
-            <span>时间: 2019-05-13 09:12</span>&nbsp;&nbsp;&nbsp;
-            <span>来源: 康安途</span>&nbsp;&nbsp;&nbsp;
-            <span>作者: 康安海外医疗</span>
+            <span>时间: {{medicalArticle.updatedDt}}</span>&nbsp;&nbsp;&nbsp;
+            <span>来源: 医诺</span>&nbsp;&nbsp;&nbsp;
+            <span>作者: 医诺海外医疗</span>
           </div>
         </div>
         <div class="content-info">
-          <div class="content" v-html="articleInfo"></div>
-          <div class="info">详情请访问:
-            <a>肿瘤: https://www.baidu.com</a>
-          </div>
+          <div class="content" v-html="medicalArticle.content"></div>
+          <!--<div class="info">详情请访问:-->
+            <!--<a>肿瘤: https://www.baidu.com</a>-->
+          <!--</div>-->
         </div>
         <div class="clare-info">
           <p style="font-size: 1.375rem;font-weight: 600">免责声明</p>
           <p style="color: #5a5a5a;font-size: 1.15rem;">由本文所表达的关于疾病的建议都不应该被视为医生的建议或替代品, 请咨询您的治疗医生了解更多细节. 本站信息权供参考, 医诺寰球不承担任何责任.</p>
         </div>
         <div class="other-info">
-          <span>上一篇: <a style="color: #005cff">肺癌晚期如何选择阿法替尼作为靶向药物进行治疗...</a></span>
-          <span>下一篇: <a style="color: #005cff">肺癌晚期如何选择阿法替尼作为靶向药物进行治疗...</a></span>
+          <span v-if="articleInfo.prevArticle">上一篇: <router-link tag="a" target="_blank" :to="'/articleInfo/'+articleInfo.prevArticle.id"  style="color: #005cff">{{articleInfo.prevArticle.title}}</router-link></span>
+          <span v-if="articleInfo.nextArticle">下一篇: <router-link tag="a" target="_blank" :to="'/articleInfo/'+articleInfo.nextArticle.id"  style="color: #005cff">{{articleInfo.nextArticle.title}}</router-link></span>
         </div>
       </div>
       <div class="right">
@@ -47,11 +47,11 @@
         </div>
         <div class="words">
           <div class="word-name">最新文章</div>
-          <div class="word-title" v-for="i in 8" :key="i">海外医疗中的靶向治疗深受晚期肺癌患者...</div>
+          <div class="word-title" v-for="(article,index) in articleInfo.newArticles" :key="index"><router-link tag="a" target="_blank" :to="'/articleInfo/'+article.id"  style="color: #005cff">{{article.title}}</router-link></div>
         </div>
         <div class="words">
           <div class="word-name">本周热门文章</div>
-          <div></div>
+          <div class="word-title" v-for="(article,index) in articleInfo.hotArticles" :key="index"><router-link tag="a" target="_blank" :to="'/articleInfo/'+article.id"  style="color: #005cff">{{article.title}}</router-link></div>
         </div>
       </div>
     </div>
@@ -59,13 +59,33 @@
 </template>
 
 <script>
+  import articleApi from '@/api/articlesFront'
   export default {
     name: 'index',
     data() {
       return {
+        medicalArticle: {
+          title: '',
+          abstractText: '',
+          content: '',
+          author: '',
+          abstractImg: ''
+        },
+        articleInfo: {},
         imgUrl: 'static/img/info/医诺寰球公众号二维码.jpg',
-        articleInfo: '<div style="color: #008aff;font-size: 4rem;text-align: center">文章详情HTML</div>',
         items: ['乙肝新闻', '丙肝新闻', '肿瘤新闻', '试管婴儿新闻', '赴美生子新闻', '心脏支架新闻', '眼角膜新闻', 'HPV疫苗新闻', '智能诊疗新闻', '癌症早筛新闻', '海外医疗新闻', '男科新闻', '周边新闻', '糖尿病新闻', '风湿免疫新闻']
+      }
+    },
+    created() {
+      this.initData()
+    }, methods: {
+      initData() {
+        articleApi.findFrontOne(this.$route.params.id).then(data => {
+          this.medicalArticle = data.obj
+        })
+        articleApi.findFrontInfo(this.$route.params.id).then(data => {
+          this.articleInfo = data.obj
+        })
       }
     }
   }
